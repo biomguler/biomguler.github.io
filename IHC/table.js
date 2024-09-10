21bdda16fd6f235e71f6f7841b0f339199574716
biomguler <muratgmbg@gmail.com>
2024-09-10 17:10:00 +0200

    update

---
+++ IHC/table.js
@@ -0,0 +1,25 @@
+// table.js
+
+// Function to initialize the DataTable
+function initializeDataTable(jsonUrl, tableId, columnsConfig, toggleConfig) {
+    $(document).ready(function() {
+        $.getJSON(jsonUrl, function(data) {
+            var table = $(tableId).DataTable({
+                data: data,
+                columns: columnsConfig,
+                orderCellsTop: true,
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ]
+            });
+
+            // Add toggle column functionality
+            for (const [checkboxId, columnIndex] of Object.entries(toggleConfig)) {
+                $(checkboxId).on('change', function () {
+                    table.column(columnIndex).visible(this.checked);
+                });
+            }
+        });
+    });
+}
